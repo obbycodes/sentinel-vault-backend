5f656ad505f42f67106652702ea28354893ad47e
obbycodes <hydrolukelx@gmail.com>
2026-09-13 12:58:27 +0100

    test: Add simple_hashing_test.py to validate password hashing and verification functionality

---
+++ tests/simple_hashing_test.py
@@ -0,0 +1,20 @@
+import sys
+
+sys.path.append(
+    "c:/Users/hydro/OneDrive/Program Files/SentinelVaultRefactor/sentinel-vault-backend/src"
+)
+
+from security import hash_password, verify_password
+
+RAW_PASSWORD = str(input("Enter a password: "))
+hashed_password = hash_password(RAW_PASSWORD)
+
+print(f"Hashed password: {hashed_password}")
+print(f"Verifying password: {verify_password(RAW_PASSWORD, hashed_password)}")
+
+wrong_password = str(input("Now enter a wrong password: "))
+print(f"Verifying wrong password: {verify_password(wrong_password, hashed_password)}")
+print("[TEST ENDED] Exiting terminal.")
+print("-" * 20)
+
+sys.exit()
