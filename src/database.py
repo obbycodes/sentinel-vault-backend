745c88ad4122b9e774c232d84dca0c8a9ad70b40
obbycodes <hydrolukelx@gmail.com>
2026-09-13 12:22:30 +0100

    fix: Minor indentation change in get_db function and ensure proper closure of database session

--- src/database.py
+++ src/database.py
@@ -5,13 +5,14 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sentinel-vault.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    )
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-def get_db(): ## Helper dependency to get a database session
+
+def get_db():
     db = SessionLocal()
     try:
         yield db
